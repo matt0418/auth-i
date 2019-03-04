ed6fe04c6b2771d9da1a9faebc547e2b2ffbfaeb
matt0418 <mfeldma7@gmail.com>
2019-03-04 16:11:21 -0500

    Changed condiitonal access to users to a middleware function

--- api/server.js
+++ api/server.js
@@ -2,11 +2,34 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const bcrypt = require('bcryptjs')
+// const router = require('express').Router()
+const Users = require('../userRoutes/helperFunctions')
+
 const registerRouter = require('../userRoutes/registerRoutes')
 const loginRouter = require('../userRoutes/loginRoutes')
 const userRouter = require('../userRoutes/userRoutes')
 
 const server = express()
+
+async function restricted(req, res, next) {
+    const {username, password} = req.headers
+    if (username && password) {
+        try {
+            user = await Users.findBy({ username }).first()
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next()
+            } else {
+                res.status(401).json({ message: "* Angry Wizard Noises *" })
+            }
+        } catch(error) {
+            console.log(error)
+            res.status(500).json(error)
+        }
+    } else {
+        res.send({ message: "Please provide both username and password" })
+    }
+}
 
 server.use(helmet())
 server.use(express.json())
@@ -14,6 +37,6 @@ server.use(cors())
 
 server.use('/api/register', registerRouter)
 server.use('/api/login', loginRouter)
-server.use('/api/users', userRouter)
+server.use('/api/users', restricted, userRouter)
 
 module.exports = server
--- userRoutes/userRoutes.js
+++ userRoutes/userRoutes.js
@@ -6,24 +6,34 @@ const Users = require('./helperFunctions')
 //     res.send({message: "more work"})
 // })
 
+// router.get('/', async (req, res) => {
+//     let { username, password } = req.headers
+//     if (username && password) {
+//         try {
+//             const user = await Users.findBy({ username }).first()
+//                 if (user && bcrypt.compareSync(password, user.password)) {
+//                     const users = await Users.find()
+//                     res.status(200).json(users)
+//                 } else {
+//                     res.status(401).json({ message: "* Angry Wizard Noises *" })
+//                 }
+//         } catch(error) {
+//             console.log(error)
+//             res.status(500).json(error)
+//         }
+//     } else {
+//         res.send({ message: "Please provide both username and password" })
+//     }
+// })
+
 router.get('/', async (req, res) => {
-    let { username, password } = req.headers
-    if (username && password) {
-        try {
-            const user = await Users.findBy({ username }).first()
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    const users = await Users.find()
-                    res.status(200).json(users)
-                } else {
-                    res.status(401).json({ message: "* Angry Wizard Noises *" })
-                }
-        } catch(error) {
-            console.log(error)
-            res.status(500).json(error)
-        }
-    } else {
-        res.send({ message: "Please provide both username and password" })
-    }
+   try {
+       const users = await Users.find()
+       res.status(200).json(users)
+   }   catch(error) {
+       console.log(error)
+       res.status(500).json(error)
+   }
 })
 
 module.exports = router
